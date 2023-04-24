2b4b2a5b7f609b37a74c0f5346cfa4877b4a30c3
Matheus Nachtigall <69644855+MatheusNachtigall@users.noreply.github.com>
2023-04-24 16:40:28 -0300

    chatcompletionstream

---
+++ internal/usecase/chatcompletionstream/completion.go
@@ -0,0 +1,44 @@
+package chatcompletionstream
+
+import (
+	"github.com/MatheusNachtigall/fclx/chatservice/internal/domain/gateway"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type ChatCompletionConfigInputDTO struct {
+	Model                string
+	ModelMaxTokenx       int
+	Temperature          float32
+	TopP                 float32
+	N                    int
+	Stop                 []string
+	MaxTokens            int
+	PresencePenalty      float32
+	FrequencePenalty     float32
+	InitialSystemMessage string
+}
+
+type ChatCompletionInputDTO struct {
+	ChatID      string
+	UserID      string
+	UserMessage string
+	Config      ChatCompletionConfigInputDTO
+}
+
+type ChatCompletionOutputDTO struct {
+	ChatID  string
+	UserID  string
+	Content string
+}
+
+type ChatCompletionUseCase struct {
+	ChatGateway  gateway.ChatGateway
+	OpenAiClient *openai.Client
+}
+
+func NewChatCompletionUseCase(chatGateway gateway.ChatGateway, openAiClient *openai.Client) *ChatCompletionUseCase {
+	return &ChatCompletionUseCase{
+		ChatGateway:  chatGateway,
+		OpenAiClient: openAiClient,
+	}
+}
