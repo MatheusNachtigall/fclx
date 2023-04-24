431a0bfae2ee101095c5694351d60aeea33f1ec8
Matheus Nachtigall <69644855+MatheusNachtigall@users.noreply.github.com>
2023-04-24 14:35:26 -0300

    First Entity structuring

---
+++ internal/domain/entity/chat.go
@@ -0,0 +1,31 @@
+package entity
+
+type ChatConfig struct {
+	Model *Model
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func NewModel(name string, maxTokens int) *Model {
+	return &Model{
+		Name:      name,
+		MaxTokens: maxTokens,
+	}
+}
+
+func (m *Model) GetMaxTokens() int {
+	return m.MaxTokens
+}
+
+func (m *Model) GetModelName() string {
+	return m.Name
+}
